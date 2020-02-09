8cec2faf25f8ed450986ab4b7c7f301be22d4620
shutovai <Pentalgin09@mail.ru>
2020-02-09 12:15:55 +0300

    StringPractice

---
+++ src/main/java/strings/StringPractice.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        String str = "Я замечательная строка";
+        System.out.println(str.substring(2));
+        System.out.println(str);
+    }
+}
